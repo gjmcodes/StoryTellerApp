b36e710553e3ed4d68073ab9c36ee71a74827e17
gjmcodes <gayan.justo@gmail.com>
2018-09-16 15:37:04 -0300

    . Implementação de Carousel para Actions de Rooms

--- StoryTellerTemplate/StoryTellerTemplate/ViewModels/MainPageViewModel.cs
+++ StoryTellerTemplate/StoryTellerTemplate/ViewModels/MainPageViewModel.cs
@@ -3,10 +3,16 @@ using Prism.Navigation;
 using StoryTellerTemplate.Interfaces.Services.GameContent;
 using StoryTellerTemplate.Interfaces.ViewModels;
 using StoryTellerTemplate.Interfaces.Views;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 
 namespace StoryTellerTemplate.ViewModels
 {
+    public class RoomActionsTest
+    {
+        public string Name { get; set; }
+    }
+
     public class MainPageViewModel : ViewModelBase, ICustomTextBindingViewModel
     {
         private readonly IGameContentAppService _gameContentAppService;
@@ -19,8 +25,16 @@ namespace StoryTellerTemplate.ViewModels
 
             NextPageCommand = new DelegateCommand(async() => await LoadDataAsync());
             _gameContentAppService = gameContentAppService;
+
+            RoomActions = new ObservableCollection<RoomActionsTest>()
+            {
+                new RoomActionsTest(){Name="Action 1"},
+                new RoomActionsTest(){Name="Action 2"},
+                new RoomActionsTest(){Name="Action 3"},
+            };
         }
 
+        public ObservableCollection<RoomActionsTest> RoomActions { get; }
         public DelegateCommand NextPageCommand { get; }
 
         public override async void OnNavigatedTo(NavigationParameters parameters)
